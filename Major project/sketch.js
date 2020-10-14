de47e3fbc0ec5eaafbb0cdcf451999a927f7ccef
unianlai <72883120+unianlai@users.noreply.github.com>
2020-10-15 05:15:24 +1100

    Update sketch.js

--- Major project/sketch.js
+++ Major project/sketch.js
@@ -146,14 +146,14 @@ var Parameters = function(){
     this.font = "./fonts/AvenirNextLTW01-Medium.woff"
     this.message = 'space';
     this.spacing = 8;
-    this.size = 250;
+    this.size = 450;
 
     this.background = [0,0,0,150]; 
     this.color = [255,255,255];
     this.strokeWeight = 0.5;
     this.threshold = 50;
     
-    this.xoffset = windowWidth/2 - this.size
+    this.xoffset = windowWidth - this.size
     this.yoffset = windowHeight/2
 
     console.log(this.xoffset)
