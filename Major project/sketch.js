384805a6da923a1b83349c182251d85104e8886d
unianlai <72883120+unianlai@users.noreply.github.com>
2020-10-15 10:13:04 +1100

    Update sketch.js

--- Major project/sketch.js
+++ Major project/sketch.js
@@ -153,7 +153,7 @@ var Parameters = function(){
     this.threshold = 50;
     
     this.xoffset = windowWidth/6
-    this.yoffset = windowHeight*2/4.1
+    this.yoffset = windowHeight*2/3.9
 
     console.log(this.xoffset)
     console.log(this.yoffset)
