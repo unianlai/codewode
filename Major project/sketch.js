aaefc4244cfc641287baa62f478e6d9d357eb23d
unianlai <72883120+unianlai@users.noreply.github.com>
2020-10-15 05:03:42 +1100

    Update sketch.js

--- Major project/sketch.js
+++ Major project/sketch.js
@@ -144,9 +144,9 @@ function Particle(x,y,tx,ty){
 var Parameters = function(){
 
     this.font = "./fonts/AvenirNextLTW01-Medium.woff"
-    this.message = 'Space';
-    this.spacing = 16;
-    this.size = 300;
+    this.message = 'space';
+    this.spacing = 5;
+    this.size = 200;
 
     this.background = [0,0,0,150]; 
     this.color = [255,255,255];
